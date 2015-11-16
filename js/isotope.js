898334a0358994353edad1a2d97c582c09a8af63
Morteza Faghih Shojaei <morteza@averta.net>
2015-11-16 10:14:56 +0330

    override resize method from outlayer.js

--- js/isotope.js
+++ js/isotope.js
@@ -626,6 +626,23 @@ var getText = docElem.textContent ?
     return this._mode().needsResizeLayout();
   };
 
+  // override resize method from outlayer
+  Isotope.prototype.resize = function() {
+    // don't trigger if size did not change
+    // or if resize was unbound. See #9
+    if ( !this.isResizeBound || !this.needsResizeLayout() ) {
+      return;
+    }
+
+    // disable transition effect on page resize
+    if ( !this.options.resizeTransition ) {
+      this._noTransition( this.layout );
+    } else {
+      this.layout();
+    }
+
+  }
+
   // -------------------------- adding & removing -------------------------- //
 
   // HEADS UP overwrites default Outlayer appended
